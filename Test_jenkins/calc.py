2da9e601e72cedf4865dfac56fe680c6f37df453
Abdellah2019 <50951980+Abdellah2019@users.noreply.github.com>
2022-11-22 11:35:27 +0100

    Add files via upload

---
+++ Test_jenkins/calc.py
@@ -0,0 +1,14 @@
+def somme(a,b):
+	return a+b
+
+
+
+def produit(a,b):
+	return a*b
+
+def soustraction(a,b):
+	return a-b
+
+
+print(somme(1,2))
+print(produit(1,2))
---
+++ Test_jenkins/calc_test.py
@@ -0,0 +1,12 @@
+import calc
+def test_somme():
+    output = calc.somme(2,4)
+    assert output == 6
+ 
+def test_produit():
+    output = calc.produit(2, 4)
+    assert output == 8
+     
+def test_soustraction():
+    output = calc.soustraction(2,4)
+    assert output == -1
